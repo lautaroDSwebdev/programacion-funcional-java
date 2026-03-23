f9c84e4a2ca6d8c17766dc7c68f1e7ad2ad2edb6
Lautaro Di Salvo <lautarodisalvo2023@gmail.com>
2026-03-23 12:17:45 -0300

    feat: Stram Collectors

--- src/steam_collectors/StreamCollectors.java
+++ src/steam_collectors/StreamCollectors.java
@@ -1,6 +1,8 @@
 package steam_collectors;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamCollectors {
@@ -10,13 +12,46 @@ public class StreamCollectors {
 
         List<String> names = List.of("Hernan", "Lautaro", "Florencia", "Roberto");
 
-
         List<String> namesFiltered = names.stream()
                 .filter(e -> e.contains("a"))
                 .collect(Collectors.toList());
-
-        System.out.println(namesFiltered);
+///        System.out.println(namesFiltered);
+///        Elimina los que por condicion no contienen una "a"
 ///    Devuelve-> [Hernan, Lautaro, Florencia]
+
+        List<String> languajes = List.of("Typescript", "Typescript", "Python", "C Sharp", "Javascript", "Rust");
+        Set<String> languajesWithoutRepeat = languajes.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toSet());
+///        System.out.println(languajesWithoutRepeat);
+///        Lo ordena alfabeticamente y sin repeticiones
+///    Devuelve-> [JAVASCRIPT, C SHARP, RUST, TYPESCRIPT, PYTHON]
+
+
+        List<String> languajes2 = List.of("Typescript", "Typescript", "Python", "C Sharp", "Javascript", "Rust");
+        String listaLang = languajes2.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.joining(" - "));
+
+        /// Coloca un elemento string de separacion entre cada una de las propiedades
+        ///  System.out.println(listaLang);
+        ///  Devuelve -> TYPESCRIPT - TYPESCRIPT - PYTHON - C SHARP - JAVASCRIPT - RUST
+
+        Long contadorCant = languajes2.stream()
+                .filter(e -> e.startsWith("T"))
+                .collect(Collectors.counting());
+
+        System.out.println(contadorCant);
+///  Cuenta la cantidad de veces existe tal caracter
+/// Devuelve -> 2
+
+        List<Integer> nums = List.of(65, 678, 43, 87, 56, 98);
+
+        Map<Boolean, List<Integer>> mayoresQue = nums.stream()
+                .collect(Collectors.partitioningBy(e -> e >= 60));
+        System.out.println(mayoresQue);
+/// Devuelve -> {false=[43, 56], true=[65, 678, 87, 98]}
+
     }
 
 
